d426d8051bdcf4e6b56cb593925da3c864c69a7e
Lakshya Agarwal <lakshayagarwal3214@gmail.com>
2020-08-13 12:46:25 +0530

    Add files via upload

---
+++ Session1/src/MyName.java
@@ -0,0 +1,11 @@
+
+public class MyName {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This code is to print my name in Hindi....");
+		System.out.println("My name in english is Lakshya Agarwal");
+		// Using unicodes to print the name in different languages
+		System.out.println("My name in hindi is \u0932\u0915\u094D\u0937\u094D\u092F"+" "+"\u0905\u0917\u0943\u0935\u093E\u0932");
+	}
+
+}
---
+++ Session1/src/Print.java
@@ -0,0 +1,9 @@
+
+public class Print {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("First code in Java");
+		System.out.println("Just a basic code to print. Looks good :)");
+	}
+
+}
